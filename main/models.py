196946ecb86b3cbb053da32bb41bf2c76a13ac32
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2024-06-22 16:31:31 +0500

    save change
    save change

--- main/models.py
+++ main/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 
+import os
+from datetime import datetime
+
+
+def upload_to(instance, filename):
+    # Get current date
+    now = datetime.now()
+    # Define the upload path
+    year_month = now.strftime('%Y-%m')  # Format: YYYY-MM
+
+    # Modify filename (example: adding timestamp)
+    filename_without_ext, ext = os.path.splitext(filename)
+    timestamp = now.strftime('%Y%m%d%H%M%S')
+    new_filename = f"{timestamp}{ext}"
+
+    return os.path.join('uploads', year_month, new_filename)
+
 
 class Category(models.Model):
     name = models.CharField(verbose_name='category', max_length=50)
@@ -21,4 +38,18 @@ class Magazen(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Announcement(models.Model):
+    name = models.CharField(max_length=255, verbose_name="name")
+    author = models.CharField(max_length=255, verbose_name="author")
+    image = models.ImageField(upload_to='image/', default='image/image.jpg')
+    content = RichTextUploadingField(verbose_name="Content")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 
